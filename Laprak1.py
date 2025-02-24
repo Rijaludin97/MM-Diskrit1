cd9b7f39d3b4338861d2027f3a390e407404f7f0
Rijaludin97 <240306036.mhs@uinmataram.ac.id>
2025-02-24 19:32:19 +0800

    Laprak1.py

--- Laprak1.py
+++ Laprak1.py
@@ -1,40 +1,38 @@
 print("TABEL KEBENARAN KONJUNGSI")
 print("-------------------------")
-
-print("n\|P\t|Q\t|P AND Q|")
+print("P\t|Q\t|P AND Q")
 print("-------------------------")
 
-for P in [True,False]:
-    for Q in [True,False]:
+for P in [True, False]:
+    for Q in [True, False]:
         Konjungsi = P and Q
         print(f"{P}\t{Q}\t{Konjungsi}")
 
-print("\n|P\t|Q\t|P OR Q")
+print("\nTABEL KEBENARAN DISJUNGSI")
 print("-------------------------")
-print("TABEL KEBENARAN DISJUNGSI")
+print("P\t|Q\t|P OR Q")
 print("-------------------------")
 
-for P in [True,False]:
-    for Q in [True,False]:
+for P in [True, False]:
+    for Q in [True, False]:
         Disjungsi = P or Q
         print(f"{P}\t{Q}\t{Disjungsi}")
-        
-print("\n|P\t|Q\t|P NOT Q")
-print("-------------------------")
-print("TABEL KEBENARAN NEGASI")
-print("----------------------") 
 
-for P in [True,False]:
-    for Q in [True,False]:
-        Negasi = not P
-        print(f"{P}\t{Q}\t{Negasi}")
-
-print("\n|P\t|Q\t|-P OR Q")
-print("-------------------------")
-print("TABEL KEBENARAN NEGASI")
+print("\nTABEL KEBENARAN NEGASI")
+print("----------------------")
+print("P\t|NOT P")
 print("----------------------")
 
-for P in [True,False]:
-    for Q in [True,False]:
+for P in [True, False]:
+    Negasi = not P
+    print(f"{P}\t{Negasi}")
+
+print("\nTABEL KEBENARAN NEGASI (Kombinasi)")
+print("-------------------------")
+print("P\t|Q\t|NOT P OR Q")
+print("-------------------------")
+
+for P in [True, False]:
+    for Q in [True, False]:
         Negasi = not P or Q
-        print(f"{P}\t{Q}\t{Negasi}")  
+        print(f"{P}\t{Q}\t{Negasi}")
